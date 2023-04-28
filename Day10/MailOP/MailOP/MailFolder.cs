ab25333ab07fc4f1bdbeeb2886bd4fad96d628de
Anaswara248948 <129931702+Anaswara248948@users.noreply.github.com>
2023-04-28 14:25:21 +0530

    Add files via upload

---
+++ Day10/MailOP/MailOP/MailFolder.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace MailOP
+{
+    public class MailFolder
+    {
+        private string _name;
+        public string Name { get { return _name; } set { _name = value; } }
+
+        private List<Mail> _mailList = new List<Mail>();
+        public List<Mail> MailList { get {  return _mailList; } set { _mailList = value; } }
+
+        public MailFolder() 
+        { 
+        }
+
+        public MailFolder(string name, List<Mail> mailList)
+        {
+            Name = name;
+            MailList = mailList;
+        }
+
+        public void AddMailToFolder(Mail mail)
+        {
+            MailList.Add(mail);
+            Console.WriteLine("Mail Successfully added");
+        }
+
+
+        public bool RemoveMailFromFolder(long id)
+        {
+            int count = 0;
+            foreach (var item in MailList)
+            {
+                if (item.Id == id)
+                {
+                    MailList.Remove(item);
+
+                    count = 1;
+                    break;
+                }
+            }
+            if (count == 1)
+            {
+                Console.WriteLine("Mail Successfully deleted");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Mail not found in the folder");
+                return false;
+            }
+        }
+
+        public void DisplayMails()
+        {
+            
+            
+                if (MailList.Count == 0)
+                {
+                    Console.WriteLine("No mails to show");
+                }
+                else
+                {
+                    Console.WriteLine("Mail in {0}\n", MailList);
+                    Console.WriteLine("{0} {1,15} {2,15} {3,15} {4,15} {5,15} {6,15}", "Id", "From", "To", "Subject", "Content", "Received Date", "Size");
+                }
+            
+
+            
+            foreach(var mail in MailList)
+            {
+                Console.WriteLine(mail);
+            }
+        }
+
+    }
+}
---
+++ Day10/MailOP/MailOP/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MailOP
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Mail> ls = new List<Mail>();
+
+            Console.WriteLine("Enter the name of the folder : ");
+            string name=Console.ReadLine();
+            MailFolder mf = new MailFolder (name, ls);
+            Mail m=new Mail ();
+
+            while (true)
+            {
+                Console.WriteLine("1. Add Mail");
+                Console.WriteLine("2. Delete Mail");
+                Console.WriteLine("3. Display Mails");
+                Console.WriteLine("4. Exit");
+
+                Console.WriteLine("Enter the choice : ");
+                int ch=int.Parse(Console.ReadLine());                
+                
+                switch(ch)
+                {
+                    case 1:
+                        //MailFolder m1 = new MailFolder(folderName,ls);
+                        Console.WriteLine("Enter the details of mail in CSV format:");
+                        string s1 = Console.ReadLine();
+                        m=Mail.CreateMail(s1);
+                        mf.AddMailToFolder(m);
+
+                                                                        
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Enter the id to be removed : ");
+                        long b=long.Parse(Console.ReadLine());
+
+                        mf.RemoveMailFromFolder(b);
+
+                        break;
+
+                    case 3:
+                        Console.WriteLine($"Mail in {name}");
+                         mf.DisplayMails();
+                        //Console.WriteLine("Mail Successfully added");
+                        break;
+
+                    case 4:
+                        break;
+
+                    default:
+                        Console.WriteLine("Enter valid choice");
+                        break;
+                }
+
+            }
+            
+        }
+    }
+}
