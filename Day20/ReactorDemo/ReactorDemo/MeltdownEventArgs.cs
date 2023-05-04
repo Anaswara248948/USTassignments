7a66468aa6d79dcdfe9d6261df84d974487d985f
Anaswara248948 <129931702+Anaswara248948@users.noreply.github.com>
2023-05-04 17:55:23 +0530

    Add files via upload

---
+++ Day20/ReactorDemo/ReactorDemo/MeltdownEventArgs.cs
@@ -0,0 +1,14 @@
+﻿namespace ReactorDemo
+{
+    public class MeltdownEventArgs
+    {
+        private string v;
+
+        public MeltdownEventArgs(string v)
+        {
+            this.v = v;
+        }
+
+        public bool Message { get; internal set; }
+    }
+}
