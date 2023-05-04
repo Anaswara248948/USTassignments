a1b310142e555441585ffae7343fcf59da0f90ce
Anaswara248948 <129931702+Anaswara248948@users.noreply.github.com>
2023-05-04 17:49:21 +0530

    Add files via upload

---
+++ Day17/SongGroup/Requirement4/PopularityComparer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Requirement4
+{
+    public class PopularityComparer : IComparer<Song>
+    {
+        public int Compare(Song x, Song y)
+        {
+            return x.NumberOfDownloads.CompareTo(y.NumberOfDownloads);
+        }
+    }
+}
---
+++ Day17/SongGroup/Requirement4/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Requirement4
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Name\t Artist\t SongType\t Rating\t NoOfDownloads\t DateDownloaded");
+            List<Song> ls = SongRepo.GetAllSongs();
+            foreach (Song s in ls)
+            {
+                Console.WriteLine(s);
+            }
+            Console.WriteLine();
+
+
+            ls.Sort(new RatingComparer());
+
+            Console.WriteLine("sorted list");
+
+            Console.WriteLine("Name\t Artist\t SongType\t Rating\t No.Of Downloads\t Date of Download");
+
+            foreach (Song s in ls)
+            {
+                Console.WriteLine(s);
+            }
+            Console.WriteLine();
+
+
+
+            ls.Sort(new PopularityComparer());
+
+            Console.WriteLine("sorted list");
+
+            Console.WriteLine("Name\t Artist\t SongType\t Rating\t No.Of Downloads\t Date of Download");
+
+            foreach (Song s in ls)
+            {
+                Console.WriteLine(s);
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
