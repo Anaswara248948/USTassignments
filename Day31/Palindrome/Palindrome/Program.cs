2e7f3f7be083fb65929f2629eaa8a9a279118831
Anaswara248948 <129931702+Anaswara248948@users.noreply.github.com>
2023-05-16 10:12:42 +0530

    Add files via upload

---
+++ Day31/Palindrome/Palindrome/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a no : ");
+            int n=int.Parse(Console.ReadLine());
+            bool rslt = IsPalindrome(n);
+
+            if (rslt == true)
+                Console.WriteLine("Number is palindrome");
+            else
+                Console.WriteLine("Number not a palindrome");
+        }
+
+        public static bool IsPalindrome(int n)
+        {
+            bool rslt;
+            int rev = 0, rem, temp;
+            temp = n;
+            while(n>0)
+            {
+                rem=n%10;
+                rev = rev * 10 + rem;
+                n= n/10;
+            }
+
+            if(rev==temp)
+                rslt= true;
+            else
+                rslt= false;
+
+            return rslt;
+        }
+    }
+}
---
+++ Day31/TwoSum/TwoSum/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoSum
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter no of elements in an array : ");
+            int n=int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the target : ");
+            int target =int.Parse(Console.ReadLine());
+            int[] nums = new int[n];            
+            Console.WriteLine("Enter array elements");
+            for(int i=0;i<n;i++)
+            {
+                nums[i]=int.Parse(Console.ReadLine());
+            }
+
+            int[] rslt = TwoSum(nums, target);
+            Console.WriteLine("Indices : [{0}, {1}]", rslt[0], rslt[1]);
+        }
+
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            int[] rslt = new int[2];
+            for(int i=0;i<nums.Length;i++)
+            {
+                for(int j=i+1; j<nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        rslt[0] = i;
+                        rslt[1] = j;                        
+                    }
+                }
+            }
+            return rslt;
+        }
+    }
+}
