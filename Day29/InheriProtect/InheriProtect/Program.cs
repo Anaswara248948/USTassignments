db3eff9b1547fd60e496edfa46ab178477fec18e
Anaswara248948 <129931702+Anaswara248948@users.noreply.github.com>
2023-05-16 10:10:16 +0530

    Add files via upload

---
+++ Day29/InheriProtect/InheriProtect/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheriProtect
+{
+    class Animal
+    {
+        protected string name="luca";
+        public void display()
+        {
+            Console.WriteLine(name);
+        }
+    }
+    class dog : Animal
+    {
+        static void Main(string[] args)
+        {
+
+            dog d = new dog();
+            d.display();
+        }
+        
+    }
+    
+}
