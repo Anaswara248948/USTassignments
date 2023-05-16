02b332159fa60d168d6ad79367a39beb894b7ec8
Anaswara248948 <129931702+Anaswara248948@users.noreply.github.com>
2023-05-16 10:11:47 +0530

    Add files via upload

---
+++ Day30/ExceptionDemo1/ExceptionDemo1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionDemo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter a number: ");
+
+                var num = int.Parse(Console.ReadLine());
+
+                Console.WriteLine($"Squre of {num} is {num * num}");
+            }
+            catch (Exception ex)
+            {
+                Console.Write("Error info:" + ex.Message);
+            }
+            finally
+            {
+                Console.Write("\nRe-try with a different number.");
+            }
+        }
+    }
+}
