1d76f33df4cacfa86f9dba2557f160b75bee5fcb
Anaswara248948 <129931702+Anaswara248948@users.noreply.github.com>
2023-05-22 14:01:54 +0530

    Add files via upload

---
+++ Day34/string1/string1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace string1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "dotnet programming";
+            foreach (var c in str)
+            {
+                 Console.WriteLine(c);
+            }
+
+            //str.Replace(' ', '$');
+            //Console.WriteLine(str);
+        }
+    }
+}
