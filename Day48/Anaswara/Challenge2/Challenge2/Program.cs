3128a0b795078e4bd33fb828595be3ea63b9c9a6
Anaswara248948 <129931702+Anaswara248948@users.noreply.github.com>
2023-06-03 17:06:28 +0530

    Add files via upload

---
+++ Day48/Anaswara/Challenge2/Challenge2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        object[] mixedArray = { 1, 2, 3, "Hello", "World" };
+
+        PrintArray(mixedArray);
+    }
+
+    static void PrintArray(object[] array)
+    {
+        foreach (object element in array)
+        {
+            Console.WriteLine(element);
+        }
+    }
+}
